0c96a6609e170e09de15915617cf5775ef17e4d1
Md. Faysal Mahmud Abid <fmaabid@gmail.com>
2017-02-26 00:48:23 +0600

    Add files via upload

---
+++ ASM Lab-4/sequence1.asm
@@ -0,0 +1,67 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+ 
+ VAR1 DB ?
+ VAR2 DW ?
+ VAR3 DW ?
+ COUNT DW 32H
+
+.CODE
+ 
+ 
+ MAIN PROC
+    
+    MOV AX, @DATA
+    MOV DS,AX       ; DS= data segment register
+    
+    MOV AH,1
+    INT 21H        ;input
+    MOV BL,AL
+    MOV VAR1,BL
+    
+    SUB BL, 30H
+    MOV VAR2,BX
+    
+    
+    
+    MOV AH, 2
+    MOV DL, 0AH ;New line
+    INT 21H
+    MOV DL, 0DH ;Move cret
+    INT 21H
+     
+    
+        OUTER:
+            XOR CX,CX
+            MOV CX,VAR2
+            DEC VAR2
+            CMP VAR2,0
+            JL LOOPOUT
+            
+        
+        INEER: 
+            MOV AH, 2
+            MOV DL, '*'
+            INT 21H
+               
+            
+            LOOP INEER 
+            
+            MOV DL, 0AH ;New line
+            INT 21H
+            MOV DL, 0DH ;Move cret
+            INT 21H 
+            
+            LOOP OUTER
+            
+  
+                        
+     
+    LOOPOUT: 
+     
+              
+              
+ MAIN ENDP 
+END MAIN  
